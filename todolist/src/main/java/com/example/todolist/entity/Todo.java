c4515fe03766c14d03e0e528fa87710a26296a13
zPaulof7 <jose.paulo.freitas.da.silva.farias@ccc.ufcg.edu.br>
2026-05-07 17:55:05 -0300

    tentativa 2

---
+++ todolist/src/main/java/com/example/todolist/entity/Todo.java
@@ -0,0 +1,51 @@
+package com.example.todolist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private int prioridade;
+    private boolean concluida;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public int getPrioridade() {
+        return prioridade;
+    }
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+    public boolean isConcluida() {
+        return concluida;
+    }
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    
+}

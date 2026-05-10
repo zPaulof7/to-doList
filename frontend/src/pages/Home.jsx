98babc81d87623c3b2a67de73f3146510d23d444
zPaulof7 <jose.paulo.freitas.da.silva.farias@ccc.ufcg.edu.br>
2026-05-10 00:45:20 -0300

    inicio da pagina inicial

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import './../styles/Home.css'
+
+function Home(){
+    return(
+        <>
+            <header>
+                <span>g</span>
+                <nav>
+                    <ul className="item">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Home</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
+export default Home
